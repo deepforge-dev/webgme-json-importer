e3e23225e7e25284bdbb9b7087e013d82b477c2a
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2022-10-03 11:56:00 -0500

    WIP- export GMEDiff as well

--- lib/common/JSONImporter.ts
+++ lib/common/JSONImporter.ts
@@ -3,8 +3,9 @@ import {OmittedProperties} from './JSONImporter/OmittedProperties';
 import {NodeSelections} from './JSONImporter/NodeSelectors';
 import {NodeChangeSet} from './JSONImporter/NodeChangeSet';
 import {NodeSelector} from './JSONImporter/NodeSelectors';
+import {gmeDiff} from './JSONImporter/SortedChanges';
 
 import diff from 'changeset';
 
 export default Importer;
-export {OmittedProperties, NodeSelections, diff, NodeChangeSet, NodeSelector};
+export {OmittedProperties, NodeSelections, diff, NodeChangeSet, NodeSelector, gmeDiff};
--- src/common/JSONImporter.js
+++ src/common/JSONImporter.js
@@ -4,13 +4,14 @@
 define([
     './build/JSONImporter.umd'
 ], function (ImporterLib) {
-    const {default: JSONImporter,  diff, NodeSelections, NodeChangeSet, NodeSelector, OmittedProperties} = ImporterLib;
+    const {default: JSONImporter,  diff, NodeSelections, NodeChangeSet, NodeSelector, OmittedProperties, gmeDiff} = ImporterLib;
 
     JSONImporter.diff = diff;
     JSONImporter.NodeChangeSet = NodeChangeSet;
     JSONImporter.NodeSelections = NodeSelections;
     JSONImporter.NodeSelector = NodeSelector;
     JSONImporter.OmittedProperties = OmittedProperties;
+    JSONImporter.gmeDiff = gmeDiff;
 
     return JSONImporter;
 });
