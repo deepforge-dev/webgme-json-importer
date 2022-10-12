8a49a377e44cf184da97ae01cbf4be15784b1b2e
umesh-timalsina <umesh-timalsina@users.noreply.github.com>
2022-10-12 21:39:06 +0000

    Prettified Code!

--- src/common/JSONImporter.js
+++ src/common/JSONImporter.js
@@ -1,10 +1,15 @@
 /* globals define */
 /* eslint-env browser, node */
 
-define([
-    './build/JSONImporter.umd'
-], function (ImporterLib) {
-    const {default: JSONImporter, NodeSelections, NodeChangeSet, NodeSelector, OmittedProperties, gmeDiff} = ImporterLib;
+define(['./build/JSONImporter.umd'], function (ImporterLib) {
+    const {
+        default: JSONImporter,
+        NodeSelections,
+        NodeChangeSet,
+        NodeSelector,
+        OmittedProperties,
+        gmeDiff,
+    } = ImporterLib;
 
     JSONImporter.NodeChangeSet = NodeChangeSet;
     JSONImporter.NodeSelections = NodeSelections;
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import {defineConfig} from 'vite';
-import {resolve} from 'path';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
@@ -13,5 +13,5 @@ export default defineConfig({
         sourcemap: true,
         outDir: 'src/common/build',
     },
-    plugins: [dts()]
+    plugins: [dts()],
 });
